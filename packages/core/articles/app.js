589d552c5ef94db29801426865e036aa269243ca
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-12-14 14:22:17 -0500

    move create form to list view, force 'anonymous' permission

--- packages/core/articles/app.js
+++ packages/core/articles/app.js
@@ -25,11 +25,6 @@ Articles.register(function(app, auth, database, circles, swagger) {
     'title': 'Articles',
     'link': 'all articles'
   });
-  Articles.menus.add({
-    'roles': ['authenticated'],
-    'title': 'Create New Article',
-    'link': 'create article'
-  });
 
   Articles.events.defaultData({
     type: 'post',
